94aac2cb8bbd7ccd1b42aa7dd165d08c59422ea6
Agbl <gabriela.souza.investimentos@gmail.com>
2025-06-25 16:04:51 -0300

    feat: Estilizando a section POOL e adicionando JQUERY

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#box_adc h3").click(function(){
+        $("#text_adc ul li").fadeToggle(1000);
+    }),
+
+    $("#box_jg h3").click(function(){
+        $("#text_jg ul li").fadeToggle(1000);
+    })
+});
