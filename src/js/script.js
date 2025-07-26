b691cc97fa17ea39022f989faa2bcd02d153bdbe
Gabriela Rodrigues de Souza <gabriela.souza.investimentos@gmail.com>
2025-07-26 17:40:13 -0300

    feat: Adicionando lógica de mudança de tema com jquery

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
 
     $("#box_jg h3").click(function(){
         $("#text_jg ul").toggleClass("open");
+    }),
+
+    $('#toggle_theme').click(function(){
+        $('body').toggleClass('light_mode');
+
+        if($('body').hasClass('light_mode')){
+            $(this).text('Modo Escuro');
+        } else {
+            $(this).text('Modo Claro');
+        }
     })
 });
